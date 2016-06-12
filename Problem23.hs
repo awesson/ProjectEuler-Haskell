7b11eae4d7d729c3f8f142da383495bde71ec660
awesson <ajwesson0@gmail.com>
2016-06-11 23:42:29 -0400

    super slow solution to 23
    Still need to find a way to memoize the fact that a number is abundant
    and take advantage of lazy evaluation

---
+++ Problem23.hs
@@ -0,0 +1,23 @@
+half num = num `quot` 2
+firstHalfList num = [1..(half num)]
+secondHalfList num = [(half num + 1)..(num - 1)]
+
+isDivisor num d = (num `mod` d) == 0
+
+properDivisors num = filter (isDivisor num) (firstHalfList num)
+
+isAbundant num = sum (properDivisors num) > num
+
+secondHalfListWDup num
+    | odd num = secondHalfList num
+    | otherwise = half num : secondHalfList num
+pairSums num = zip (firstHalfList num) (reverse $ secondHalfListWDup num)
+
+containsAbundantPair ((n1, n2):xs)
+    | not (isAbundant n1) || not (isAbundant n2) = containsAbundantPair xs
+    | otherwise = True
+containsAbundantPair [] = False
+isComposedOfAbundantPairSum num = containsAbundantPair $ pairSums num
+
+maxNonAbundantPairNum = 1000
+sumAllNonAbundantPairNums = sum $ filter (not . isComposedOfAbundantPairSum) [1..maxNonAbundantPairNum]
