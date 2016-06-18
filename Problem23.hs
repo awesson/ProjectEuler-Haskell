ae3380c426580d561639f9d52933d7614fa06cae
awesson <ajwesson0@gmail.com>
2016-06-18 16:49:45 -0400

    Loop over pairs of abundant numbers instead
    Reduced the search space by instead of checking if each number can be
    made from a pair of abundant numbers by checking each abundant number
    less than it and it's compliment, generate all numbers that are the sum
    of a pair of the known abundant numbers. Then sum all numbers not in
    that set.

--- Problem23.hs
+++ Problem23.hs
@@ -1,26 +1,35 @@
 import Data.List
+import qualified Data.IntSet as IntSet
 
 smallestAbundantNum = 12
 maxNonAbundantPairNum = 28123
 
-half = (`quot` 2)
 upToSqrtList num = [2..(truncate (sqrt $ fromIntegral num))]
 
 d `isDivisor` num = (num `mod` d) == 0
 
+-- Check if each number less than the sqrt evenly divides into the number.
 properDivisors num = foldl' (properDivisorsHelper num) [1] (upToSqrtList num)
     where properDivisorsHelper num acc val
+            -- Add the pair of divisors when it does,
+            -- unless the pair is the sqrt in which case just add it once.
             | val `isDivisor` num && val == (num `quot` val) = val:acc
             | val `isDivisor` num = val:(num `quot` val):acc
             | otherwise = acc
 
+-- Use the index in a map in order to make this a CAF and get cached
 isAbundant = (!!) (map abundantDef [0..])
     where abundantDef num = sum (properDivisors num) > num
 
-abundantNums = filter isAbundant [smallestAbundantNum..(half maxNonAbundantPairNum)]
+abundantNums = filter isAbundant [smallestAbundantNum..maxNonAbundantPairNum]
 
-isComposedOfAbundantPairSum num = foldl' (anyAbundantSoFar num) False (relevantAbundantNums num)
-    where anyAbundantSoFar num acc val = acc || isAbundant (num - val)
-          relevantAbundantNums num = takeWhile (<= half num) abundantNums
+-- Essentially a loop over each abundant number and then an inner loop that
+-- pairs the current number with each abundant number less than it,
+-- including itself. This will give each unique pair of abundant numbers,
+-- which then get's summed together and added to an IntSet to remove duplicates
+-- and make queries fast.
+abundantPairNums = foldl' innerLoop IntSet.empty abundantNums
+    where innerLoop acc val = foldl' (insertSum val) acc (takeWhile (<= val) abundantNums)
+            where insertSum outerNum acc val = IntSet.insert (val + outerNum) acc
 
-sumAllNonAbundantPairNums = sum $ filter (not . isComposedOfAbundantPairSum) [1..maxNonAbundantPairNum]
+sumAllNonAbundantPairNums = sum $ filter (`IntSet.notMember` abundantPairNums) [1..maxNonAbundantPairNum]
